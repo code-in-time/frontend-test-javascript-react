7d758b1e29f5a03700656da81f2cc015884574aa
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-02-19 21:10:48 +0200

    Add random number test

--- src/utils/helpers.test.js
+++ src/utils/helpers.test.js
@@ -1,9 +1,9 @@
 import { getRandomNumber } from './helpers';
 
 test('Should get a random number greater or equal to 1', () => {
-  expect(getRandomNumber(87)).toBeGreaterThanOrEqual(1);
+  expect(getRandomNumber(5)).toBeGreaterThanOrEqual(1);
 });
 
-test('Should get a random number less than or equal to 87', () => {
-  expect(getRandomNumber(87)).toBeLessThanOrEqual(87);
+test('Should get a random number less than or equal to 5', () => {
+  expect(getRandomNumber(5)).toBeLessThanOrEqual(5);
 });
