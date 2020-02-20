0978f9e9c5edd7062573874633728c61e8bf3caf
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-02-20 21:08:59 +0200

    Add test for pickTwoUniqueNumbers and add note

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 
 ## To the run the game
 - npm run start
+
+### NOTE 20/02/2020
+ - Updated the helpers.test.js with more detailed tests
--- src/utils/helpers.test.js
+++ src/utils/helpers.test.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from './helpers';
+import { getRandomNumber, pickTwoUniqueNumbers } from './helpers';
 
 test('Should get a random number greater or equal to 1 and less than or equal to 5', () => {
   // Generate 10 random numbers
@@ -34,4 +34,29 @@ test('Should get a random number greater or equal to 1 and less than or equal to
   expect(n8).toBeLessThanOrEqual(5);
   expect(n9).toBeLessThanOrEqual(5);
   expect(n10).toBeLessThanOrEqual(5);
+});
+
+test('Should pick two unique numbers that are not equal and are from 1 to 5', () => {
+  // Generate 10 random number pairs
+  const a = pickTwoUniqueNumbers(5);
+  const b = pickTwoUniqueNumbers(5);
+  const c = pickTwoUniqueNumbers(5);
+  const d = pickTwoUniqueNumbers(5);
+  const e = pickTwoUniqueNumbers(5);
+  const f = pickTwoUniqueNumbers(5);
+  const g = pickTwoUniqueNumbers(5);
+  const h = pickTwoUniqueNumbers(5);
+  const i = pickTwoUniqueNumbers(5);
+  const j = pickTwoUniqueNumbers(5);
+  // Test that the numbers are not equal
+  expect(a.n1 !== a.n2 ).toBeTruthy();
+  expect(b.n1 !== b.n2 ).toBeTruthy();
+  expect(c.n1 !== c.n2 ).toBeTruthy();
+  expect(d.n1 !== d.n2 ).toBeTruthy();
+  expect(e.n1 !== e.n2 ).toBeTruthy();
+  expect(f.n1 !== f.n2 ).toBeTruthy();
+  expect(g.n1 !== g.n2 ).toBeTruthy();
+  expect(h.n1 !== h.n2 ).toBeTruthy();
+  expect(i.n1 !== i.n2 ).toBeTruthy();
+  expect(j.n1 !== j.n2 ).toBeTruthy();
 });
